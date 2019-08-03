ac2f90158d299684d9a09b1333143d4c24e4436d
Fabrício <fabricio-gf@hotmail.com>
2019-08-03 14:38:33 -0300

    Added match manager

---
+++ Assets/Scripts/MatchManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    public bool[] connectedPlayers;
+
+    public PlayerSpawner playerSpawner;
+
+    GameObject[] playerReferences;
+
+    public EnemySpawner enemySpawner;
+    public PointsTracker pointsTracker;
+
+    public void Start()
+    {
+        
+    }
+
+    public void StartCountdown()
+    {
+        //player spawn animation
+        //toggle points trackers
+
+        //start countdown
+
+        //on end, match start
+    }
+
+    public void OnMatchStart()
+    {
+        //start enemy spawns
+        //allow inputs
+        //start auto shooting
+    }
+}
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public Vector3[] spawnPositions;
+
+    public GameObject[] playerPrefabs;
+
+    public Transform playerParent;
+
+    GameObject obj;
+
+    public void SpawnPlayers(bool[] connectedPlayers)
+    {
+        for(int i = 0; i < connectedPlayers.Length; i++)
+        {
+            if (connectedPlayers[i])
+            {
+                obj = Instantiate(playerPrefabs[i], spawnPositions[i], Quaternion.identity, playerParent);
+            }
+        }
+    }
+}
