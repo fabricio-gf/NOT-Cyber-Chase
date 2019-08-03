e1726bf5af1bc85e397beaa5b2ef8ecf0698d790
Fabrício <fabricio-gf@hotmail.com>
2019-08-03 11:13:31 -0300

    Added basic player HUD

--- Assets/Scripts/PointsTracker.cs
+++ Assets/Scripts/PointsTracker.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class PointsTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int[] points;
+    public TextMeshProUGUI[] pointsTexts;
 
-    // Update is called once per frame
-    void Update()
+    public void UpdatePoints(int change, int index)
     {
-        
+        points[index] += change;
+        pointsTexts[index].text = points[index].ToString();
     }
 }
