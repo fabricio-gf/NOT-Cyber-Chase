817f3bb7a28fbab45aad9209b74b701fd98b6e04
Fabrício <fabricio-gf@hotmail.com>
2019-08-03 09:44:53 -0300

    Added basic enemy spawn

--- Assets/Scripts/Killzone.cs
+++ Assets/Scripts/Killzone.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Killzone : MonoBehaviour
 {
-    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        
     }
 }
