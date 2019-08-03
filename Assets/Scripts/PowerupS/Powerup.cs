6947d3f07d2a7b150336fa0cdf8da45c441d8caa
Fabrício <fabricio-gf@hotmail.com>
2019-08-03 10:49:19 -0300

    Added basic powerup spawn and behaviour

---
+++ Assets/Scripts/PowerupS/Powerup.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    float angularVelocity;
+    public float maxVelocity;
+    public float minVelocity;
+
+    public PowerupType myType;
+
+    public enum PowerupType
+    {
+        SHOT,
+        SHIELD,
+        BOMB,
+        LIFE
+    }
+
+    public void Awake()
+    {
+        angularVelocity = Random.Range(minVelocity, maxVelocity);
+        if(Random.value >= 0.5f)
+        {
+            angularVelocity = -angularVelocity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1), angularVelocity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player pl = collision.GetComponent<Player>();
+            switch (myType)
+            {
+                case PowerupType.SHOT:
+                    pl.IncrementShotLevel();
+                break;
+                case PowerupType.SHIELD:
+                    pl.AddShield();
+                break;
+                case PowerupType.BOMB:
+                    pl.AddBomb();
+                break;
+                case PowerupType.LIFE:
+                    pl.AddLife();
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PowerupS/PowerupSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    public GameObject[] powerUpPrefabs;
+    public Transform spawnParent;
+
+    public float baseSpawnRate;
+    public float spawnIncrement;
+
+    float spawnRate;
+
+    void CheckPowerupSpawn(Vector3 position)
+    {
+        if(Random.value <= spawnRate)
+        {
+            Instantiate(powerUpPrefabs[Random.Range(0, powerUpPrefabs.Length)], position, Quaternion.identity, spawnParent);
+            spawnRate = baseSpawnRate;
+        }
+        else
+        {
+            spawnRate += spawnIncrement;
+        }
+    }
+}
