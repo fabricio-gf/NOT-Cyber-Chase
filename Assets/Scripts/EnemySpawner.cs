70d85e86ba0bbe6b7a6636986932c2017f80f99e
Fabrício <fabricio-gf@hotmail.com>
2019-08-03 09:22:35 -0300

    Added text mesh pro and basic scripts

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Vector3[] spawnPositions;
+
+    public GameObject[] enemies;
+
+    public float spawnDelay;
+
+    public float difficultyMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
