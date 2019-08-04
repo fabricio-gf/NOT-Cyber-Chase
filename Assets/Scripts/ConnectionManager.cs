695d241364582a931790702b916a0d6fccefcdeb
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-08-03 22:22:50 -0300

    Comecei a implementar o sistema de input manager

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionManager : MonoBehaviour {
+
+    public static ConnectionManager instance = null;
+
+    static List<MonoBehaviour> Connections;
+
+    void Awake() {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;
+
+        //Destroy the fake
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //Dont destroy the ruler
+        DontDestroyOnLoad(gameObject);
+        //Long live the King!
+    }
+
+    /// <summary>
+    /// Quando um NetworkBehaviour for spawnado, ele deve pedir pra ser cadastrado no network behaviour
+    /// </summary>
+    /// <param name="connection"></param>
+    public static void CreateNewConnection (MonoBehaviour connection) {
+        Connections.Add(connection);
+    }
+
+    public static void ExitConnection(MonoBehaviour connection) {
+        Connections.Remove(connection);
+    }
+
+    public enum LanInputType {
+        None,
+        Up,
+        Right,
+        Down,
+        Left,
+        Tap,
+        DoubleTap,
+        Confirmation,
+        Cancel
+    }
+
+    private void Update() {
+        foreach (MonoBehaviour connection in Connections) {
+            if (connection.input == ) {
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    // Start is called before the first frame update
+    static PlayerController Player0;
+    static PlayerController Player1;
+    static PlayerController Player2;
+    static PlayerController Player3;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    enum InputSourceType {
+        Lan,
+        Controller,
+        Arrows
+    }
+
+    public static PlayerController GetPlayerInput(int index) {
+        switch (index) {
+            case 0:
+                return Player0;
+            case 1:
+                return Player1;
+            case 2:
+                return Player2;
+            case 3:
+                return Player3;
+            default:
+                break;
+        }
+        return null;
+    }
+
+    public abstract class PlayerController {
+        protected string name;
+
+        public abstract float GetHorizontal();
+        public abstract float GetVertical();
+        public abstract bool GetConfirmation();
+        public abstract bool GetCancel();
+    }
+
+    public class LanController : PlayerController{
+        MonoBehaviour myConnection;
+        LanController(MonoBehaviour connection) {
+            myConnection = connection;
+            //name = myConnection.name;
+        }
+        
+        override
+        public float GetHorizontal() {
+            return 0f;
+        }
+
+        override
+        public float GetVertical() {
+            return 0f;
+        }
+
+        override
+        public bool GetConfirmation() {
+            return false;
+        }
+
+        override
+        public bool GetCancel() {
+            return false;
+        }
+    }
+}
