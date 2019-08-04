6f4fbdfb136c8f788dbd9f85daf0f859954f4a12
Fabrício <fabricio-gf@hotmail.com>
2019-08-04 16:33:17 -0300

    Added client game scene

--- Assets/3rd-Party/Mirror/Runtime/NetworkManagerHUD.cs
+++ Assets/3rd-Party/Mirror/Runtime/NetworkManagerHUD.cs
@@ -117,7 +117,8 @@ namespace Mirror
         public void StartClient(){
             manager.networkAddress = GameObject.Find("IpText").GetComponent<Text>().text.ToString();
             if(manager.networkAddress == ""){
-                manager.networkAddress = "localhost";
+                //manager.networkAddress = "localhost";
+                return;
             }
             manager.playerName = GameObject.Find("NameText").GetComponent<Text>().text.ToString();
             manager.StartClient();
